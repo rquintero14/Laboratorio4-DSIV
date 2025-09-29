a58f264b3f37a3e4aedd9b6ea64a6ebdba5f9d66
rquintero14 <rquinte14@gmail.com>
2025-09-29 10:39:44 -0500

    Se modifica el diseño del form 2

--- Laboratorio4/Form1.Designer.cs
+++ Laboratorio4/Form1.Designer.cs
@@ -29,16 +29,17 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.lbBienvenida = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.cbProducto = new System.Windows.Forms.ComboBox();
-            this.txtMontoC = new System.Windows.Forms.Label();
             this.txtMontoF = new System.Windows.Forms.Label();
+            this.txtMontoC = new System.Windows.Forms.Label();
+            this.cbProducto = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.btn = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rbContado = new System.Windows.Forms.RadioButton();
+            this.rbCuotas = new System.Windows.Forms.RadioButton();
+            this.cbCuotas = new System.Windows.Forms.ComboBox();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,55 +47,70 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(400, 31);
+            this.label1.Location = new System.Drawing.Point(350, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(135, 39);
+            this.label1.Size = new System.Drawing.Size(130, 38);
             this.label1.TabIndex = 0;
             this.label1.Text = "Raenco";
             // 
-            // groupBox1
+            // lbBienvenida
             // 
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(32, 106);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(296, 319);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Datos personales";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 36);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(109, 25);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Bienvenido";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 75);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(204, 25);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Nombre de la persona";
+            this.lbBienvenida.AutoSize = true;
+            this.lbBienvenida.Location = new System.Drawing.Point(52, 71);
+            this.lbBienvenida.Name = "lbBienvenida";
+            this.lbBienvenida.Size = new System.Drawing.Size(10, 16);
+            this.lbBienvenida.TabIndex = 0;
+            this.lbBienvenida.Text = ".";
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.cbCuotas);
+            this.groupBox2.Controls.Add(this.rbCuotas);
+            this.groupBox2.Controls.Add(this.rbContado);
+            this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.txtMontoF);
             this.groupBox2.Controls.Add(this.txtMontoC);
             this.groupBox2.Controls.Add(this.cbProducto);
             this.groupBox2.Controls.Add(this.label5);
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(447, 106);
+            this.groupBox2.Location = new System.Drawing.Point(44, 107);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(426, 267);
+            this.groupBox2.Size = new System.Drawing.Size(426, 369);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Datos personales";
+            // 
+            // txtMontoF
+            // 
+            this.txtMontoF.AutoSize = true;
+            this.txtMontoF.Location = new System.Drawing.Point(6, 296);
+            this.txtMontoF.Name = "txtMontoF";
+            this.txtMontoF.Size = new System.Drawing.Size(107, 25);
+            this.txtMontoF.TabIndex = 3;
+            this.txtMontoF.Text = "Monto final";
+            // 
+            // txtMontoC
+            // 
+            this.txtMontoC.AutoSize = true;
+            this.txtMontoC.Location = new System.Drawing.Point(6, 248);
+            this.txtMontoC.Name = "txtMontoC";
+            this.txtMontoC.Size = new System.Drawing.Size(153, 25);
+            this.txtMontoC.TabIndex = 2;
+            this.txtMontoC.Text = "Monto por cuota";
+            // 
+            // cbProducto
+            // 
+            this.cbProducto.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbProducto.FormattingEnabled = true;
+            this.cbProducto.Items.AddRange(new object[] {
+            "Lavadora",
+            "Nevera",
+            "Estufa"});
+            this.cbProducto.Location = new System.Drawing.Point(11, 75);
+            this.cbProducto.Name = "cbProducto";
+            this.cbProducto.Size = new System.Drawing.Size(299, 33);
+            this.cbProducto.TabIndex = 1;
+            this.cbProducto.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label5
             // 
@@ -105,61 +121,71 @@
             this.label5.TabIndex = 0;
             this.label5.Text = "¿Que producto desea comprar?";
             // 
-            // cbProducto
-            // 
-            this.cbProducto.FormattingEnabled = true;
-            this.cbProducto.Items.AddRange(new object[] {
-            "Al contado",
-            "12 cuotas",
-            "18 cuotas",
-            "36 cuotas"});
-            this.cbProducto.Location = new System.Drawing.Point(11, 75);
-            this.cbProducto.Name = "cbProducto";
-            this.cbProducto.Size = new System.Drawing.Size(299, 33);
-            this.cbProducto.TabIndex = 1;
-            this.cbProducto.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
-            // txtMontoC
-            // 
-            this.txtMontoC.AutoSize = true;
-            this.txtMontoC.Location = new System.Drawing.Point(6, 146);
-            this.txtMontoC.Name = "txtMontoC";
-            this.txtMontoC.Size = new System.Drawing.Size(153, 25);
-            this.txtMontoC.TabIndex = 2;
-            this.txtMontoC.Text = "Monto por cuota";
-            // 
-            // txtMontoF
-            // 
-            this.txtMontoF.AutoSize = true;
-            this.txtMontoF.Location = new System.Drawing.Point(6, 197);
-            this.txtMontoF.Name = "txtMontoF";
-            this.txtMontoF.Size = new System.Drawing.Size(107, 25);
-            this.txtMontoF.TabIndex = 3;
-            this.txtMontoF.Text = "Monto final";
-            // 
             // btn
             // 
-            this.btn.Location = new System.Drawing.Point(572, 402);
+            this.btn.Location = new System.Drawing.Point(560, 238);
             this.btn.Name = "btn";
             this.btn.Size = new System.Drawing.Size(207, 73);
             this.btn.TabIndex = 3;
             this.btn.Text = "button1";
             this.btn.UseVisualStyleBackColor = true;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(144, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Forma de pago";
+            // 
+            // rbContado
+            // 
+            this.rbContado.AutoSize = true;
+            this.rbContado.Location = new System.Drawing.Point(26, 161);
+            this.rbContado.Name = "rbContado";
+            this.rbContado.Size = new System.Drawing.Size(126, 29);
+            this.rbContado.TabIndex = 5;
+            this.rbContado.TabStop = true;
+            this.rbContado.Text = "Al contado";
+            this.rbContado.UseVisualStyleBackColor = true;
+            // 
+            // rbCuotas
+            // 
+            this.rbCuotas.AutoSize = true;
+            this.rbCuotas.Location = new System.Drawing.Point(26, 196);
+            this.rbCuotas.Name = "rbCuotas";
+            this.rbCuotas.Size = new System.Drawing.Size(96, 29);
+            this.rbCuotas.TabIndex = 6;
+            this.rbCuotas.TabStop = true;
+            this.rbCuotas.Text = "Cuotas";
+            this.rbCuotas.UseVisualStyleBackColor = true;
+            // 
+            // cbCuotas
+            // 
+            this.cbCuotas.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCuotas.FormattingEnabled = true;
+            this.cbCuotas.Items.AddRange(new object[] {
+            "12",
+            "18",
+            "36"});
+            this.cbCuotas.Location = new System.Drawing.Point(157, 192);
+            this.cbCuotas.Name = "cbCuotas";
+            this.cbCuotas.Size = new System.Drawing.Size(120, 33);
+            this.cbCuotas.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(961, 506);
+            this.ClientSize = new System.Drawing.Size(837, 506);
             this.Controls.Add(this.btn);
+            this.Controls.Add(this.lbBienvenida);
             this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Compra";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
@@ -170,15 +196,17 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbBienvenida;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label txtMontoF;
         private System.Windows.Forms.Label txtMontoC;
         private System.Windows.Forms.ComboBox cbProducto;
         private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton rbCuotas;
+        private System.Windows.Forms.RadioButton rbContado;
+        private System.Windows.Forms.ComboBox cbCuotas;
     }
 }
 
