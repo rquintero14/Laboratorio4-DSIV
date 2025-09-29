26d52a2a6bec7a61173c92f2173fca46f710b9d6
rquintero14 <rquinte14@gmail.com>
2025-09-29 10:16:08 -0500

    se crea el archivo de trabajo

---
+++ Laboratorio4/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Laboratorio4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbProducto = new System.Windows.Forms.ComboBox();
+            this.txtMontoC = new System.Windows.Forms.Label();
+            this.txtMontoF = new System.Windows.Forms.Label();
+            this.btn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(400, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Raenco";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(32, 106);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(296, 319);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos personales";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Bienvenido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(204, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Nombre de la persona";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtMontoF);
+            this.groupBox2.Controls.Add(this.txtMontoC);
+            this.groupBox2.Controls.Add(this.cbProducto);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(447, 106);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(426, 267);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Datos personales";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 36);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(289, 25);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "¿Que producto desea comprar?";
+            // 
+            // cbProducto
+            // 
+            this.cbProducto.FormattingEnabled = true;
+            this.cbProducto.Items.AddRange(new object[] {
+            "Al contado",
+            "12 cuotas",
+            "18 cuotas",
+            "36 cuotas"});
+            this.cbProducto.Location = new System.Drawing.Point(11, 75);
+            this.cbProducto.Name = "cbProducto";
+            this.cbProducto.Size = new System.Drawing.Size(299, 33);
+            this.cbProducto.TabIndex = 1;
+            this.cbProducto.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // txtMontoC
+            // 
+            this.txtMontoC.AutoSize = true;
+            this.txtMontoC.Location = new System.Drawing.Point(6, 146);
+            this.txtMontoC.Name = "txtMontoC";
+            this.txtMontoC.Size = new System.Drawing.Size(153, 25);
+            this.txtMontoC.TabIndex = 2;
+            this.txtMontoC.Text = "Monto por cuota";
+            // 
+            // txtMontoF
+            // 
+            this.txtMontoF.AutoSize = true;
+            this.txtMontoF.Location = new System.Drawing.Point(6, 197);
+            this.txtMontoF.Name = "txtMontoF";
+            this.txtMontoF.Size = new System.Drawing.Size(107, 25);
+            this.txtMontoF.TabIndex = 3;
+            this.txtMontoF.Text = "Monto final";
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(572, 402);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(207, 73);
+            this.btn.TabIndex = 3;
+            this.btn.Text = "button1";
+            this.btn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(961, 506);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Compra";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label txtMontoF;
+        private System.Windows.Forms.Label txtMontoC;
+        private System.Windows.Forms.ComboBox cbProducto;
+        private System.Windows.Forms.Button btn;
+    }
+}
+
